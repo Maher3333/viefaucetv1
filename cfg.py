3fa869c79f5cfe4bf9d4909e555f5f62f2e9b6b1
Maher3333 <mkml2905@gmail.com>
2024-09-07 04:36:29 +0300

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,3 +1,11 @@
-coockie = 'xxxxxxx'
-user_agent = "xxxxx"
+coockie = '_ga-GA1.2.798290816.1725672221;_gid=GA1.2.1950272294.1725672222;
+
+max-age=0
+
+_gat_gtag_UA_157714740_1=1;_ga_RBNPR7SQ94-GS1.1.1725672221.1.1.1725672248.0.0.0;
+
+bitmedia_fid=eyJmaWQiOilyNzQ1MzI1NzFiMGQxNDZIOGY4NDk3NjJiZTJjZmExNilslmZpZG5vd OilwMmVhNmY2ZDY5ZGEwNjU3OWY5ZjcxZWZjZGQ2Zml2ZiJ9'
+user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0
+
+Mobile Safari/537.36"
 bear = "xxxxxxx" #Autorization
